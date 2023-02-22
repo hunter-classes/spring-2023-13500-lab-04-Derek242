4d8e5d2b2fc3abac2b4d195a3631475a2ee28199
Derek242 <derek.ni47@myhunter.cuny.edu>
2023-02-22 13:40:41 -0500

    finished Lab 4

--- checkerboard.cpp
+++ checkerboard.cpp
@@ -10,22 +10,6 @@ std::string checkerboard(int width, int height){
 
     for (int i = 0; i < height; i++){
         for (int j = 0; j < width; j++){
-            if (i % 2 == 0){
-                //even
-                if (j % 2 == 0){
-                    x = x + "*";
-                } else {
-                  x = x + " ";
-                }
-            } 
-            else{
-                //odd
-                if (j % 2 == 0){
-                    x = x + " ";
-                } else {
-                    x = x + "*";
-                }
-                
 
             }
         }
